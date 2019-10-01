b16507ad49cfc5da85db8566f78774547bb9a7f5
BadKoldrysh <ohmygodkir17@gmail.com>
2019-10-01 20:29:10 +0200

    Add data from state.js. Modify all var to let in state.js.

--- vue-cardgame/main.js
+++ vue-cardgame/main.js
@@ -1,4 +1,14 @@
 new Vue({
     name: 'cardgame',
     el: '#app',
+    template: `<div id="#app">
+        Hello World!
+    </div>`,
+    data: state,
+
+});
+
+// Window resize handling
+window.addEventListener('resize', () => {
+    state.worldRatio = getWorldRatio();
 });
