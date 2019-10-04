25503a2aba4de311a9f3a008eb48199cd8be31a9
BadKoldrysh <ohmygodkir17@gmail.com>
2019-10-04 23:56:00 +0200

    Add binding between child and parent. Add connecting and showing cards

--- vue-cardgame/components/ui.js
+++ vue-cardgame/components/ui.js
@@ -13,3 +13,24 @@ Vue.component('top-bar', {
     },
 
 });
+
+Vue.component('card', {
+    template: `<div class="card" :class="'type-' + def.type" @click="play">
+        <div class="title">{{ def.title }}</div>
+        <img class="separator" src="svg/card-separator.svg" />
+        <div class="description">
+            <div v-html="def.description"></div>
+        </div>
+        <div class="note" v-if="def.note">
+            <div v-html="def.note"></div>
+        </div>
+    </div>`,
+    props: ['def'],
+    methods: {
+        play() {
+            // for sending event to parent
+            this.$emit('play');
+        }
+    }
+
+});
--- vue-cardgame/main.js
+++ vue-cardgame/main.js
@@ -1,10 +1,22 @@
 new Vue({
     name: 'cardgame',
     el: '#app',
+    // we have @play from child element
     template: `<div id="#app">
         <top-bar :turn="turn" :current-player-index="currentPlayerIndex" :players="players" />
+        <card :def="testCard" @play="handlePlay"  />
     </div>`,
     data: state,
+    computed: {
+        testCard() {
+            return cards.fireball;
+        }
+    },
+    methods: {
+        handlePlay() {
+            console.log('You played a card!');
+        },
+    },
 
 });
 
