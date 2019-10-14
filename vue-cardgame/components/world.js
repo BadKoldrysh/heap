2c507eb1528c542ab377e2e424ff9508e6694dcf
BadKoldrysh <ohmygodkir17@gmail.com>
2019-10-14 19:40:27 +0200

    Refactor code and modify some things

---
+++ vue-cardgame/components/world.js
@@ -0,0 +1,31 @@
+Vue.component('castle', {
+    template: `<div class="castle" :class="'player-' + index">
+        <img class="building" :src="'svg/castle' + index + '.svg'" />
+        <img class="ground" :src="'svg/ground' + index + '.svg'" />
+        <castle-banners :player="player" />    
+    </div>`,
+    props: ['player', 'index'],
+});
+
+Vue.component('castle-banners', {
+    template: `<div class="banners">
+        <!-- Food -->
+        <img class="food-icon" src="svg/food-icon.svg" />
+        <!-- Bubble here -->
+        <!-- Banner bar here -->
+
+        <!-- Health -->
+        <img class="health-icon" src="svg/health-icon.svg" />
+        <!-- Bubble here -->
+        <!-- Banner bar here -->
+    </div>`,
+    props: ['player'],
+    computes: {
+        foodRatio() {
+            return this.player.food / maxFood;
+        },
+        healthRatio() {
+            return this.player.health / maxHealth;
+        },
+    }
+});
--- vue-cardgame/main.js
+++ vue-cardgame/main.js
@@ -4,6 +4,10 @@ new Vue({
     // we have @play from child element
     template: `<div id="#app">
         <top-bar :turn="turn" :current-player-index="currentPlayerIndex" :players="players" />
+        <div class="world">
+            <castle v-for="(player, index) in players" :player="player" :index="index" />
+            <div class="land" />
+        </div>
         <transition name="hand">
             <hand :cards="testHand" v-if="!activeOverlay" @card-play="testPlayCard" />
         </transition>
