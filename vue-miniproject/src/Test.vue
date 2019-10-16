b80935e3b4f187e451b1e8669cdac95d478f3ad8
BadKoldrysh <ohmygodkir17@gmail.com>
2019-10-16 23:41:10 +0200

    Modify script

--- vue-miniproject/src/Test.vue
+++ vue-miniproject/src/Test.vue
@@ -3,9 +3,9 @@ div
     p {{ message }}
     input(type='text', v-model='message')
     ul.movies
-        li.movie Star Wars
-        li.movie The Godfather
-        li.movie Blade Runner
+        li.movie {{ movies[0].title }}
+        li.movie {{ movies[1].title }}
+        li.movie {{ movies[2].title }}
 
 </template>
 
@@ -14,6 +14,11 @@ export default {
     data() {
         return {
             message: 'Hello World!',
+            movies: [
+                { title: 'Star Wars' },
+                { title: 'The Godfather' },
+                { title: 'Blade Runner' },
+            ]
         };
     },
 }
