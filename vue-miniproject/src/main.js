7ad0acf45dcce7e6bb9a23fad033d1fd37ec4c93
BadKoldrysh <ohmygodkir17@gmail.com>
2019-10-16 21:27:08 +0200

    Change the text row to the movies list

--- vue-miniproject/src/main.js
+++ vue-miniproject/src/main.js
@@ -2,5 +2,9 @@ import Vue from 'vue';
 
 new Vue({
     el: '#app',
-    render: h => h('div', 'hello world'), 
+    render: h => h('div', {'class': 'movies'}, [
+        h('li', {'class': 'movie'}, 'Star Wars'),
+        h('li', {'class': 'movie'}, 'The Godfather'),
+        h('li', {'class': 'movie'}, 'Blade Runner')
+    ]), 
 });
