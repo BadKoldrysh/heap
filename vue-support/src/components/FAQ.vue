1708e3f86338919ed47a863480ceaf44f38e584e
BadKoldrysh <ohmygodkir17@gmail.com>
2019-10-21 23:52:15 +0200

    Prepare FAQ page for fetching the data from the server

--- vue-support/src/components/FAQ.vue
+++ vue-support/src/components/FAQ.vue
@@ -1,5 +1,30 @@
 <template>
     <main class="faq">
         <h1>Frequently Asked Questions</h1>
+    
+        <div class="error" v-if="error">
+            Can't load the question
+        </div>
+
+        <section class="list">
+            <article v-for="question of questions" :key="question.title">
+                <h2 v-html="question.title"></h2>
+                <p v-html="question.content"></p>
+            </article>
+        </section>
     </main>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            questions: [],
+            error: null,
+        };
+    },
+    created() {
+        // fetch here
+    }
+}
+</script>
