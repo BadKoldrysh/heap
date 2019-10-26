5c3a54ecb92b117340f4c698b73f27ddb0afb2d0
BadKoldrysh <ohmygodkir17@gmail.com>
2019-10-26 23:55:57 +0200

    Create the SmartForm component and add it to global components

--- vue-support/src/global-components.js
+++ vue-support/src/global-components.js
@@ -5,5 +5,7 @@
 
 import Vue from 'vue';
 import Loading from './components/Loading.vue';
+import SmartForm from './components/SmartForm.vue';
 
 Vue.component('Loading', Loading);
+Vue.component('SmartForm', SmartForm);
