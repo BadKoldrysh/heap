ae686dee1612801f8e9d5054074b925ee209e497
BadKoldrysh <ohmygodkir17@gmail.com>
2019-11-06 23:28:14 +0100

    Add main files and the database. Modify package.json file

---
+++ mocha-tests/src/user/user.repository.js
@@ -0,0 +1,24 @@
+// src/user/user.repository.js
+
+/**
+ * Repository class handles getting data into
+ * and out of out data. A repository is used between
+ * the service layer and the model layer
+ */
+const { UserModel } = require("../database");
+class UserRepository {
+    constructor() {
+        this.user = UserModel;
+        this.user.sync({ force: true });
+    }
+    async create(name, email) {
+        return this.user.create({
+            name,
+            email
+        });
+    }
+    async getUser(id) {
+        return this.user.findOne({ id });
+    }
+}
+module.exports = UserRepository;
