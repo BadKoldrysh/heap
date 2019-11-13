d5b26c16a7b68cac7cd151d610a46653dff02cc1
BadKoldrysh <ohmygodkir17@gmail.com>
2019-11-14 00:06:21 +0100

    Insall package Django and create the project in it

---
+++ python_projects/myhellowebapp/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kir\heap\python_projects\myhellowebapp\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
