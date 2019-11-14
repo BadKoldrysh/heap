df683dc37744bf7373203a5cfe6ab00fb5061722
BadKoldrysh <ohmygodkir17@gmail.com>
2019-11-15 00:08:23 +0100

    Modify homepage template with using a variable from the views.py

--- python_projects/myhellowebapp/collection/views.py
+++ python_projects/myhellowebapp/collection/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    # defining the variable
+    number = 4
+    # passing the variable to the view
     # this is your new view
-    return render(request, 'index.html')
-    
+    return render(request, 'index.html', {
+        'number': number,
+    })
+
