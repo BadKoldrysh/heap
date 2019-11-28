d342ac343095c52e04fef0ec266d05c3b10df2a7
BadKoldrysh <ohmygodkir17@gmail.com>
2019-11-28 22:44:32 +0100

    Add a few new methods for getting data

--- python_projects/myhellowebapp/collection/views.py
+++ python_projects/myhellowebapp/collection/views.py
@@ -4,6 +4,15 @@ from collection.models import Thing
 # Create your views here.
 def index(request):
     things = Thing.objects.all()
+
+    # for getting object by filter
+    # things = Thing.objects.filter(name="ok, where are my pants?").order_by("name")
+    #
+    # or another method
+    # things = Thing.objects.filter(name__contains="thing_name") 
+
+    # for getting just one object 
+    # correct_thing = Thing.objects.get(name="thing_name")
     return render(request, 'index.html', {
         'things': things,
     })
