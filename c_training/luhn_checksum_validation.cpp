710162e70db301c967dbbcadbf08ef862acc2579
BadKoldrysh <ohmygodkir17@gmail.com>
2020-02-24 21:07:40 +0100

    Create function for double digits with summing when doubled digit is bigger or equal than 10

--- c_training/luhn_checksum_validation.cpp
+++ c_training/luhn_checksum_validation.cpp
@@ -2,16 +2,24 @@
 
 using namespace std;
 
-int main() {
-    char digit;
-    int checksum = 0;
-    cout << "Enter a single digit number, 0-9: ";
-    
-    for (int i = 0; i < 5; i++) {
-        int n = cin.get();
-        if (n != 10) {
-            checksum += n - '0';
-        }
+// knowing which digits to double
+// treating doubled numbers 10 and grerater according to their individual digits
+// knowing we've reached the end of the number
+// reading each digit separately
+
+int doubledDigitValue(int digit) {
+    int doubledDigit = digit * 2;
+    if (doubledDigit >= 10) {
+        return 1 + (doubledDigit % 10);
+    } else {
+        return doubledDigit;
     }
-    cout << checksum;
+}
+
+int main() {
+    int digit;
+    cout << "Enter a single digit number, 0-9: ";
+    cin >> digit;
+    
+    cout << doubledDigitValue(digit) << "\n";
 }
