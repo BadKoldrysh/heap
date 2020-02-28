775de1559343b69838532185b4c508ced8b58aff
BadKoldrysh <ohmygodkir17@gmail.com>
2020-02-28 20:57:03 +0100

    Convert input characters set to a number

--- c_training/decodeMessage.cpp
+++ c_training/decodeMessage.cpp
@@ -13,6 +13,7 @@ using namespace std;
 //
 // * Read character by character until we reach an end-of-line
 // * Convert a series of characters representing a number to an integer
+//
 // * Convert an integer 1-26 into an uppercase letter
 // * Convert an integer 1-26 into a lowercase letter
 // * Convert an integer 1-8 into a punctuation symbol
@@ -20,17 +21,27 @@ using namespace std;
 //
 // input 18,12312,171,763,98423,1208,216,11,500,18,241,0,32,20620,27,10
 
-int main() {
-    char symbol;
-    int symCode;
-    
-    cout << "Enter a number: ";
-    symbol = cin.get();
-    while (symbol != 10) {
-        symCode = symbol - '0';
-        cout << symCode;
-        symbol = cin.get();
-    }
+void mainProgram() {
+    char character;
+    int digit;
+    int numb = 0;
 
+    cout << "Enter a number: ";
+    character = cin.get();
+    while (character != 10) {
+        // if (character == ',') {
+
+        //     break;
+        // }
+        digit = character - '0';
+        numb = numb * 10 + digit;
+
+        character = cin.get();
+    }
+    cout << "Number is " << numb; 
     cout << "\nEND\n";
+}
+
+int main() {
+    mainProgram();
 }
