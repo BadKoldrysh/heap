d523179ca92cf241e907897bff66df0f41f80946
BadKoldrysh <ohmygodkir17@gmail.com>
2020-02-28 21:58:14 +0100

    Add function for getting the symbol by the code. Finish the main function

--- c_training/decodeMessage.cpp
+++ c_training/decodeMessage.cpp
@@ -13,33 +13,76 @@ using namespace std;
 //
 // * Read character by character until we reach an end-of-line
 // * Convert a series of characters representing a number to an integer
-//
 // * Convert an integer 1-26 into an uppercase letter
 // * Convert an integer 1-26 into a lowercase letter
 // * Convert an integer 1-8 into a punctuation symbol
+//
 // * Track a decoding mode
 //
 // input 18,12312,171,763,98423,1208,216,11,500,18,241,0,32,20620,27,10
 
+char getCharByCode(int code) {
+    if (code == 1) return '!';
+    else if (code == 2) return '?';
+    else if (code == 3) return ',';
+    else if (code == 4) return '.';
+    else if (code == 5) return ' ';
+    else if (code == 6) return ';';
+    else if (code == 7) return '"';
+    else return 39; // the code of '
+}
+
 void mainProgram() {
     char character;
+    char smbl;
     int digit;
+    
     int numb = 0;
+    int mode = 0;
 
     cout << "Enter a number: ";
     character = cin.get();
     while (character != 10) {
-        // if (character == ',') {
-
-        //     break;
-        // }
-        digit = character - '0';
-        numb = numb * 10 + digit;
+        if (character == ',') {
+            int modulo = mode != 2 ? numb % 27 : numb % 9;
+            if (mode == 0) {
+                if (modulo != 0) {
+                    smbl = (modulo-1) + 'A';
+                    cout << smbl;
+                } else {
+                    mode = 1;
+                }
+            } else if (mode == 1) {
+                if (modulo != 0) {
+                    smbl = (modulo-1) + 'a';
+                    cout << smbl;
+                } else {
+                    mode = 2;
+                }
+            } else {
+                if (modulo != 0) {
+                    smbl = getCharByCode(modulo);
+                    cout << smbl;
+                } else {
+                    mode = 0;
+                }
+            } 
+            numb = 0;
+        } else {
+            digit = character - '0';
+            numb = numb * 10 + digit;
+        }
 
         character = cin.get();
     }
-    cout << "Number is " << numb; 
-    cout << "\nEND\n";
+    int modulo = mode != 2 ? numb % 27 : numb % 9;
+    if (modulo != 0) {
+        if (mode == 0) smbl = (modulo-1) + 'A';
+        else if (mode == 1) smbl = (modulo-1) + 'a';
+        else smbl = getCharByCode(modulo);
+        cout << smbl << "\n";
+    }
+    cout << "END\n";
 }
 
 int main() {
