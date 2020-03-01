f05f9833da9f0342fb356827eace073c247fef25
BadKoldrysh <ohmygodkir17@gmail.com>
2020-03-01 19:32:57 +0100

    create the first faker object and get a random email

--- node_and_mysql/app.js
+++ node_and_mysql/app.js
@@ -1,3 +1,9 @@
-for (let i = 0; i < 500; i++) {
-    console.log(i+1 + ") HELLO, MR NODE! I LIKE PHP)))");
-}
+'use strict';
+
+// for (let i = 0; i < 500; i++) {
+//     console.log(i+1 + ") HELLO, MR NODE! I LIKE PHP)))");
+// }
+
+let faker = require('faker');
+
+console.log(faker.internet.email());
