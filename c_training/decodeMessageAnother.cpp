111cfbb462eea5629b642ccf19c2667fe8510c33
BadKoldrysh <ohmygodkir17@gmail.com>
2020-03-01 15:37:29 +0100

    add the switching between modes

--- c_training/decodeMessageAnother.cpp
+++ c_training/decodeMessageAnother.cpp
@@ -2,6 +2,44 @@
 
 using namespace std;
 
+void modeSwitching() {
+    enum modeType {UPPERCASE, LOWERCASE, PUNCTUATION};
+    int number;
+    modeType mode = UPPERCASE;
+    cout << "Enter some numbers ending with -1: ";
+    do {
+        cin >> number;
+        cout << "Number read: " << number;
+        switch (mode) {
+            case UPPERCASE:
+                number = number % 27;
+                cout << ". Modulo 27: " << number << ". ";
+                if (number == 0) {
+                    cout << "Switch to LOWERCASE";
+                    mode = LOWERCASE;
+                }
+                break;
+            case LOWERCASE:
+                number = number % 27;
+                cout << ". Modulo 27: " << number << ". ";
+                if (number == 0) {
+                    cout << "Switch to PUNCTUATION";
+                    mode = PUNCTUATION;
+                }
+                break;
+            case PUNCTUATION:
+                number = number % 9;
+                cout << ". Modulo 9: " << number << ". ";
+                if (number == 0) {
+                    cout << "Switch to UPPERCASE";
+                    mode = UPPERCASE;
+                }
+                break;
+        }
+        cout << "\n";
+    } while (number != -1);
+}
+
 char convertNumberToUppercaseLetter(int number) {
     return number + 'A' - 1;
 }
@@ -33,10 +71,10 @@ void getNumbers() {
             number = number * 10 + (digitChar - '0');
             digitChar = cin.get();
         }
-        cout << "Numbered entered: " << number << "\n";
+        cout << "Numbered entered: " << number << "\n";  
     } while (digitChar != 10);
 }
 
 int main() {
-    // convertNumberToSymbol();
+    modeSwitching();
 }
