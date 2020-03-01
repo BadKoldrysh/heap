7165993ffd407535d5beb14323486836956af3fa
BadKoldrysh <ohmygodkir17@gmail.com>
2020-03-01 22:22:55 +0100

    add a simple query to the database; fix bad username for the connection

--- node_and_mysql/app.js
+++ node_and_mysql/app.js
@@ -5,9 +5,19 @@ let mysql = require('mysql');
 
 let connection = mysql.createConnection({
     host: 'localhost',
-    user: 'user',
+    user: 'root',
+    password: '',
     database: 'ig_clone',
 });
+
+let q = "SELECT 1 + 5 AS answer";
+
+connection.query(q, function(error, results, fields) {
+    if (error) throw error;
+    console.log(results[0].answer);
+});
+
+connection.end();
 
 function generateAddress()
 {
