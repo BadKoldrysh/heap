7f376f9b174304c565cf6cb4be615e8e15b7431f
BadKoldrysh <ohmygodkir17@gmail.com>
2020-03-01 19:56:22 +0100

    add the mysql package; use this package in the app script

--- node_and_mysql/app.js
+++ node_and_mysql/app.js
@@ -1,15 +1,13 @@
 'use strict';
 
-// for (let i = 0; i < 500; i++) {
-//     console.log(i+1 + ") HELLO, MR NODE! I LIKE PHP)))");
-// }
-
 let faker = require('faker');
+let mysql = require('mysql');
 
-console.log(faker.internet.email());
-console.log(faker.date.past());
-
-generateAddress();
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    database: 'ig_clone',
+});
 
 function generateAddress()
 {
@@ -18,6 +16,3 @@ function generateAddress()
     console.log(faker.address.state());
 }
 
-// for (let i = 0; i < 25; i++) {
-//     console.log(faker.internet.email());
-// }
