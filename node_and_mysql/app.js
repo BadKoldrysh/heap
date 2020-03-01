8d5b0694a6539ede4d513f6af7ab796505e74bc7
BadKoldrysh <ohmygodkir17@gmail.com>
2020-03-02 00:05:00 +0100

    add exercises

--- node_and_mysql/app.js
+++ node_and_mysql/app.js
@@ -45,28 +45,77 @@ let connection = mysql.createConnection({
 
 // connection.end();
 
-let data = [];
+// let data = [];
 
-for (let i = 0; i < 500; i++) {
-    data.push([
-        faker.internet.email(),
-        faker.date.past(),
-    ]);
-}
+// for (let i = 0; i < 500; i++) {
+//     data.push([
+//         faker.internet.email(),
+//         faker.date.past(),
+//     ]);
+// }
 
-connection.query("INSERT INTO users(email, created_at) VALUES ?", [data], function(error, results) {
-    console.log(error);
+// connection.query("INSERT INTO users(email, created_at) VALUES ?", [data], function(error, results) {
+//     console.log(error);
+//     console.log(results);
+// });
+
+// connection.end();
+
+let query = "SELECT DATE_FORMAT(MIN(created_at), '%M %D %Y') AS earliest_date FROM users";
+connection.query(query, function(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+});
+query = "SELECT * FROM users WHERE created_at = (SELECT MIN(created_at) FROM users)"
+connection.query(query, function(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+});
+query = "SELECT * FROM users ORDER BY created_at LIMIT 1";
+connection.query(query, function(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+});
+query = "SELECT MONTHNAME(created_at) AS month, COUNT(*) AS total FROM users GROUP BY month ORDER BY total DESC";
+connection.query(query, function(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    for (let i = 0; i < results.length; i++) {
+        console.log(results[i].month + " => " + results[i].total);
+    }
+});
+query = "SELECT count(*) AS yahoo_users FROM users WHERE email LIKE '%yahoo%'";
+connection.query(query, function(error, results) {
+    if (error) {
+        console.log(error);
+    }
+
+    console.log(results);
+});
+query = "SELECT " + 
+            "CASE " + 
+                "WHEN email LIKE '%gmail%' THEN 'gmail' " +
+                "WHEN email LIKE '%yahoo%' THEN 'yahoo' " +
+                "WHEN email LIKE '%hotmail%' THEN 'hotmail' " +
+                "ELSE 'other' " +
+            "END AS provider, " +
+        "COUNT(*) AS total_users " +
+        "FROM users " +
+        "GROUP BY provider " +
+        "ORDER BY total_users DESC";
+connection.query(query, function(error, results) {
+    if (error) {
+        console.log(error);
+    }
+
     console.log(results);
 });
 
 connection.end();
-
-
-
-function generateAddress()
-{
-    console.log(faker.address.streetAddress());
-    console.log(faker.address.city());
-    console.log(faker.address.state());
-}
-
--- node_and_mysql/schema.sql
+++ node_and_mysql/schema.sql
@@ -6,3 +6,15 @@ CREATE TABLE users(
 INSERT INTO users(email) VALUES
 ("katie34@yahoo.com"),
 ("tunde@gmail.com");
+
+SELECT 
+    CASE
+        WHEN email LIKE '%gmail%' THEN 'gmail'
+        WHEN email LIKE '%yahoo%' THEN 'yahoo'
+        WHEN email LIKE '%hotmail%' THEN 'hotmail'
+        ELSE 'other'
+    END AS provider,
+    count(*) AS total_users
+FROM users
+GROUP BY provider
+ORDER BY total_users DESC;
