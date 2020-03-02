d575853ee4ef5a7685839d5a921e2170dd3d0749
BadKoldrysh <ohmygodkir17@gmail.com>
2020-03-02 20:37:20 +0100

    fill the addError and validateUsername methods

--- php_projects/php_oop/UserValidator.php
+++ php_projects/php_oop/UserValidator.php
@@ -7,16 +7,19 @@
 // -- a method to validate an email
 // return an error array once all checks are done
 
-class userValidator {
+class userValidator
+{
     private $data;
     private $error = [];
     private static $fields = ['username', 'email'];
 
-    public function __construct($postData) {
+    public function __construct($postData)
+    {
         $this->data = $postData;
     }
 
-    public function validateForm() {
+    public function validateForm()
+    {
         foreach (self::fields as $field) {
             if (!array_key_exist($field, $this->data)) {
                 trigger_error("$field is not present in data");
@@ -28,15 +31,26 @@ class userValidator {
         $this->validateEmail();
     }
 
-    private function validateUsername() {
+    private function validateUsername(): void
+    {
+        $val = trim($this->data['username']);
+
+        if (empty($val)) {
+            $this->addError('username', 'username cannot be empty');
+        } else {
+            if (!preg_match('/^[a-zA-Z0-9]{6,12}$/', $val)) {
+                $this->addError('username', 'username must be 6-12 chars and username must be alphanumeric');
+            }
+        }
+    }
+
+    private function validateEmail(): void
+    {
 
     }
 
-    private function validateEmail() {
-
-    }
-
-    private function addError() {
-
+    private function addError(string $key, string $msg)
+    {
+        $this->errors[$key] = $msg;
     }
 }
