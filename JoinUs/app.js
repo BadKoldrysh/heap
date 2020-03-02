a0ce183990a6a5ca2832fcae6ff5652f1e2167f9
BadKoldrysh <ohmygodkir17@gmail.com>
2020-03-02 23:05:56 +0100

    add a few new pages and a simple math ops

--- JoinUs/app.js
+++ JoinUs/app.js
@@ -3,6 +3,18 @@ let app = express();
 
 app.get("/", function(request, result) {
     result.send("HELLO FROM EXPRESS WEB APP");
+    console.log("HOME PAGE HAS REQUESTED");
+});
+
+app.get("/contacts", function(req, res) {
+    let message = "IT'S A CONTACTS PAGE";
+    res.send(message);
+    console.log("CONTACT PAGE HAS REQUESTED")
+});
+
+app.get("/rand_num", function(req, res) {
+    let n = Math.floor(Math.random() * 10) + 1;
+    res.send("Your lucky number is " + n);
 });
 
 app.listen(8080, function() {
