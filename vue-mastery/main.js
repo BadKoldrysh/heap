607933f195c2b29885aaba5066989150504bd7f6
BadKoldrysh <ohmygodkir17@gmail.com>
2020-04-16 20:09:46 +0200

    create an vue-mastery project; very easy start with Vue; yes, I like tutorials :))))

---
+++ vue-mastery/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "SOCKS",
+        description: 'a pair of warm, fuzzy socks',
+        image: "./assets/vmSocks-green.jpg",
+        altText: "here may be a pair of socks",
+        linkText: "click here, if you can",
+        linkToPast: "https://www.virgin.com/"
+    }
+});
