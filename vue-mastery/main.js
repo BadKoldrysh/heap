e55321d355dc5af16c43b2951806bae794a7b5f4
BadKoldrysh <ohmygodkir17@gmail.com>
2020-04-18 11:54:45 +0200

    try of using of class and style bindings

--- vue-mastery/main.js
+++ vue-mastery/main.js
@@ -3,37 +3,44 @@ var app = new Vue({
     data: {
         product: "SOCKS",
         description: 'a pair of warm, fuzzy socks',
-        image: "./assets/vmSocks-green.jpg",
+        image: "./assets/green.jpg",
         altText: "here may be a pair of socks",
         linkText: "click here, if you can",
         linkToPast: "https://www.virgin.com/",
         isPremium: true,
         inventory: 5,
         onSale: true,
+        inStock: true,
         details: ["80% cotton", "really green color", "best present for boyfriend/girlfriend"],
         sizes: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
         variants: [
             {
                 variantId: 1908,
-                variantName: "Black socks",
-                variantImage: "./assets/vmSocks-green.jpg"
+                variantName: "Blue socks",
+                variantImage: "./assets/blue.jpg",
+                variantColor: "#1e95ea",
             },
             {
                 variantId: 3908,
-                variantName: "Red socks",
-                variantImage: "./assets/vmSocks-green.jpg"
+                variantName: "Green socks",
+                variantImage: "./assets/green.jpg",
+                variantColor: "#1eea73",
             },
             {
                 variantId: 3103,
                 variantName: "Blue socks",
-                variantImage: "./assets/vmSocks-blue.jpg"
+                variantImage: "./assets/blue.jpg",
+                variantColor: "#1e95ea",
             },
             {
                 variantId: 2023,
                 variantName: "Green socks",
-                variantImage: "./assets/vmSocks-green.jpg"
+                variantImage: "./assets/green.jpg",
+                variantColor: "#1eea73",
             },
         ],
+        disableCl: "disabledButton",
+        disableCl2: "disabledCursor",
         counter: 0,
     },
     methods: {
@@ -46,8 +53,11 @@ var app = new Vue({
         resetCounter() {
             this.counter = 0;
         },
-        changeSocksImage(socksImage) {
-            this.image = socksImage;
+        modifyImage(img) {
+            this.image = img;
+        },
+        addToCart() {
+            console.log("you are right");
         }
     }
 });
