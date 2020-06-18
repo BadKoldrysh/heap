ef534f645fe7f235f776c2c6b24dc2923a1e5094
BadKoldrysh <ohmygodkir17@gmail.com>
2020-06-18 19:22:07 +0200

    mdify game-prototype

--- games/prototype/game.js
+++ games/prototype/game.js
@@ -1,12 +1,29 @@
 const gameArea = {
     canvas: document.createElement("canvas"),
+    context: () => this.context,
     start: function() {
-        this.canvas.width = 1024;
-        this.canvas.height = 768;
+        this.canvas.width = 800;
+        this.canvas.height = 600;
         this.canvas.setAttribute("id", "gameArea");
+        this.gameObjects = [];
         this.context = this.canvas.getContext("2d");
+
         document.body.appendChild(this.canvas);
-    }
+
+        setInterval(() => {
+            this.updateGame();
+        }, 20);
+    },
+    addObject: function(obj) {
+        this.gameObjects.push(obj);
+    },
+    updateGame: function() {
+        // console.log(this.gameObjects);
+        this.gameObjects.forEach((o) => {
+            o.update();
+        })
+    },
+    gameObjects: () => this.gameObjects,
 };
 
 
@@ -14,6 +31,23 @@ startGame();
 
 function startGame() {
     gameArea.start();
+    const hero = new Component(30, 30, "#900", 50, 25);
+    gameArea.addObject(hero);
+}
+
+class Component {
+    constructor(width, height, color, x, y) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+
+        this.update = function () {
+            ctx = gameArea.context;
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        };
+    }
 }
 
 
